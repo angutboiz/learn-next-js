ea032b43eb74485960cbfd8952369054ae1358bb
angutboiz <thngan25k3@gmail.com>
2023-08-22 09:07:57 +0700

    add name in h1

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1>HJeloo</h1>
+			<h1>HJeloo My name is trongandev from with love :3 </h1>
 		</main>
 	)
 }
